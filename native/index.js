2aed23566c23c391499eea4121f1273ea5c59925
Ranjan Shrestha <ranjan@sharingapples.com>
2018-01-31 13:00:58 +0545

    Add react-native wrapper

---
+++ native/index.js
@@ -0,0 +1,49 @@
+/* global WebSocket */
+import createSocket from 'socket.red-client';
+import { NetInfo, AppState } from 'react-native';
+
+const EVENT = 'connectionChange';
+
+export default function createNativeSocket(dispatch, options) {
+  let handler = null;
+  const Network = {
+    isConnected: async () => NetInfo.isConnected.fetch(),
+
+    onConnected: (listener) => {
+      // Only one network listener should be used, so remove
+      // any existing
+      if (handler) {
+        NetInfo.isConnected.removeEventListener(EVENT, handler);
+      }
+
+      // Create a new handler
+      handler = (isConnected) => {
+        if (isConnected) {
+          NetInfo.isConnected.removeEventListener(EVENT, handler);
+          listener();
+        }
+      };
+
+      NetInfo.isConnected.addEventListener(EVENT, handler);
+    },
+
+    clear: () => {
+      if (handler) {
+        NetInfo.isConnected.removeEventListener(EVENT, handler);
+      }
+    },
+  };
+
+  const socket = createSocket(dispatch, options, WebSocket, Network);
+
+  AppState.addEventListener('change', (state) => {
+    // As soon as the app goes to the background state, close the socket
+    if (state === 'background') {
+      socket.freeze();
+    } else if (state === 'active') {
+      socket.unfreeze();
+    }
+  });
+
+  return socket;
+}
