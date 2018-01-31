b9abdb490708724378730a9d1842d56ed8126138
Ranjan Shrestha <ranjan@sharingapples.com>
2018-01-31 17:19:25 +0545

    Fix test cases and include test case to check for session error handling

--- server/__tests__/server.spec.js
+++ server/__tests__/server.spec.js
@@ -1,43 +1,79 @@
+
 const startServer = require('../src/server');
 const WebSocket = require('ws');
 
+const port = 8001;
+
 describe('WebSocket Server Specification', () => {
-  it('must start properly', async () => {
-    const session = {
-      end: () => {},
-      api: {
-        fn1: jest.fn(),
-        sum: (a, b) => a + b,
-        err: (msg) => {
-          throw new Error(msg);
-        },
+  let stop = null;
+  const session = {
+    onClose: () => {},
+    api: {
+      fn1: jest.fn(),
+      sum: (a, b) => a + b,
+      err: (msg) => {
+        throw new Error(msg);
       },
-    };
-    const stop = await startServer(8002, (url) => {
+    },
+  };
+
+  beforeAll(async () => {
+    stop = await startServer(port, (url) => {
+      if (url === '/err') {
+        return null;
+      }
+
       session.url = url;
 
-      setImmediate(() => {
+      session.onStart = () => {
         session.emit('eee', 'John');
         session.dispatch({ type: 'ACTION', payload: 'Doe' });
-      });
+      };
+
       return session;
     });
+  });
 
-    const socket = new WebSocket('ws://localhost:8002');
-    socket.on('open', () => {
-      socket.send(JSON.stringify([1, 'fn1', [1, 2, 3]]));
-      socket.send(JSON.stringify([2, 'fn2', [4]]));
-      socket.send(JSON.stringify([3, 'sum', [4, 5]]));
-      socket.send(JSON.stringify([4, 'err', ['Echo Error']]));
-    });
+  afterAll(() => {
+    stop();
+  });
 
-    socket.onclose = jest.fn();
-    socket.onerror = jest.fn();
-    socket.onmessage = jest.fn();
-
+  it('must start properly', async () => {
     return new Promise((resolve) => {
-      setTimeout(() => {
-        stop();
+      const socket = new WebSocket(`ws://localhost:${port}`);
+
+      function send(obj) {
+        return new Promise((sendResolve, reject) => {
+          socket.send(JSON.stringify(obj), (err) => {
+            if (err) {
+              return reject(err);
+            }
+
+            return sendResolve();
+          });
+        });
+      }
+
+      socket.onerror = jest.fn();
+
+      socket.onmessage = jest.fn();
+
+      let count = 0;
+      socket.on('message', () => {
+        count += 1;
+        if (count === 6) {
+          socket.close();
+        }
+      });
+
+      socket.on('open', () => {
+        send([1, 'fn1', [1, 2, 3]]);
+        send([2, 'fn2', [4]]);
+        send([3, 'sum', [4, 5]]);
+        send([4, 'err', ['Echo Error']]);
+      });
+
+      socket.on('close', () => {
         expect(typeof session.dispatch).toBe('function');
         expect(typeof session.emit).toBe('function');
 
@@ -51,11 +87,23 @@ describe('WebSocket Server Specification', () => {
         expect(messages[3]).toEqual([2, false, 'Unknown api \'fn2\'']);
         expect(messages[4]).toEqual([3, true, 9]);
         expect(messages[5]).toEqual([4, false, 'Echo Error']);
-        // console.log(socket.onmessage.mock.calls);
-        // expect(socket.onerror.mock.calls.length).toBe(0);
+        expect(socket.onerror.mock.calls.length).toBe(0);
         expect(session.url).toBe('/');
+
         resolve();
-      }, 100);
+      });
     });
   });
+
+  it('must handle error', async () => new Promise((resolve) => {
+    const socket = new WebSocket(`ws://localhost:${port}/err`);
+    socket.onopen = jest.fn();
+    socket.onerror = jest.fn();
+
+    socket.on('close', () => {
+      expect(socket.onopen.mock.calls.length).toBe(0);
+      expect(socket.onerror.mock.calls.length).toBe(1);
+      resolve();
+    });
+  }));
 });
