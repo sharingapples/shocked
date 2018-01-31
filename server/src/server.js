cb190a68a3c38d35d4a0ad117e44236475731659
Ranjan Shrestha <ranjan@sharingapples.com>
2018-01-31 12:34:46 +0545

    Add repository

---
+++ server/src/server.js
@@ -0,0 +1,78 @@
+const WebSocket = require('ws');
+
+function noop() { }
+
+function beat() {
+  this.isAlive = true;
+}
+
+module.exports = function start(server, createSession, pulseRate = 0) {
+  const options = (typeof server === 'number') ? ({ port: server }) : server;
+  const wss = new WebSocket.Server(options);
+
+  function keepAlive() {
+    wss.clients.forEach((ws) => {
+      if (!ws.isAlive) {
+        return ws.terminate();
+      }
+
+      // eslint-disable-next-line no-param-reassign
+      ws.isAlive = false;
+      return ws.ping(noop);
+    });
+  }
+
+  let heartBeat = null;
+  if (pulseRate > 0) {
+    heartBeat = setInterval(keepAlive, pulseRate);
+  }
+
+  wss.on('connection', (ws, req) => {
+    const session = createSession(req.url);
+
+    session.dispatch = (action) => {
+      ws.send(JSON.stringify([0, action]));
+    };
+
+    session.emit = (event, data) => {
+      ws.send(JSON.stringify([-1, event, data]));
+    };
+
+    if (heartBeat) {
+      ws.on('pong', beat);
+    }
+
+    ws.on('close', () => session.end());
+    ws.on('message', async (data) => {
+      try {
+        const msg = JSON.parse(data);
+        if (!Array.isArray(msg)) {
+          throw new Error('Socket only supports array messages');
+        }
+
+        const [code, name, args] = msg;
+        const fn = session[name];
+        if (code > 0) {
+          try {
+            const res = await fn.apply(session, args);
+            ws.send(JSON.stringify([code, true, res]));
+          } catch (err) {
+            ws.send(JSON.stringify([code, false, err.message]));
+          }
+        } else {
+          fn.apply(session, args);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  return () => {
+    if (heartBeat) {
+      clearInterval(heartBeat);
+    }
+    wss.clients.forEach(ws => ws.close());
+    wss.close();
+  };
+};
