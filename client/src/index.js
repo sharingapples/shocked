ac433a8e2187c7689161792d508a01c6005a011a
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-01 10:25:28 +0545

    Add and expose a helper method to connect socket with RemoteAPI

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 const createSocket = require('./Socket');
+const connectApi = require('./connectApi');
 
 module.exports = createSocket;
+exports.connectApi = connectApi;
 exports.default = createSocket;
