4190ee116a3a4d41cd5aec89d00a0f87535fd8c2
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-02 11:53:14 +0545

    Export library as named exports

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 const createSocket = require('./Socket');
 const connectApi = require('./connectApi');
 
-module.exports = createSocket;
-exports.connectApi = connectApi;
 exports.default = createSocket;
+module.exports = {
+  createSocket,
+  connectApi,
+};
