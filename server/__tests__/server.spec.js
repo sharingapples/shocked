4c590dcb9b7bf5049c1bf33ab4af34e3cf674b93
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-13 15:04:47 +0545

    Fix lint error and avoid using onClose

--- server/__tests__/server.spec.js
+++ server/__tests__/server.spec.js
@@ -14,7 +14,6 @@ describe('WebSocket Server Specification', () => {
     },
   };
   const session = {
-    onClose: () => {},
     onStart: () => {
       session.emit('eee', 'John');
       session.dispatch({ type: 'ACTION', payload: 'Doe' });
@@ -38,62 +37,60 @@ describe('WebSocket Server Specification', () => {
     stop();
   });
 
-  it('must start properly', async () => {
-    return new Promise((resolve) => {
-      const socket = new WebSocket(`ws://localhost:${port}`);
+  it('must start properly', async () => new Promise((resolve) => {
+    const socket = new WebSocket(`ws://localhost:${port}`);
 
-      function send(obj) {
-        return new Promise((sendResolve, reject) => {
-          socket.send(JSON.stringify(obj), (err) => {
-            if (err) {
-              return reject(err);
-            }
+    function send(obj) {
+      return new Promise((sendResolve, reject) => {
+        socket.send(JSON.stringify(obj), (err) => {
+          if (err) {
+            return reject(err);
+          }
 
-            return sendResolve();
-          });
+          return sendResolve();
         });
+      });
+    }
+
+    socket.onerror = jest.fn();
+
+    socket.onmessage = jest.fn();
+
+    let count = 0;
+    socket.on('message', () => {
+      count += 1;
+      if (count === 6) {
+        socket.close();
       }
-
-      socket.onerror = jest.fn();
-
-      socket.onmessage = jest.fn();
-
-      let count = 0;
-      socket.on('message', () => {
-        count += 1;
-        if (count === 6) {
-          socket.close();
-        }
-      });
-
-      socket.on('open', () => {
-        send([1, 'fn1', [1, 2, 3]]);
-        send([2, 'fn2', [4]]);
-        send([3, 'sum', [4, 5]]);
-        send([4, 'err', ['Echo Error']]);
-      });
-
-      socket.on('close', () => {
-        expect(typeof session.dispatch).toBe('function');
-        expect(typeof session.emit).toBe('function');
-
-        expect(api.fn1.mock.calls.length).toBe(1);
-        const messages = socket.onmessage.mock.calls.map(c => JSON.parse(c[0].data));
-        messages.sort((a, b) => a[0] - b[0]);
-        expect(messages.length).toBe(6);
-        expect(messages[0]).toEqual([-1, 'eee', 'John']);
-        expect(messages[1]).toEqual([0, { type: 'ACTION', payload: 'Doe' }]);
-        expect(messages[2]).toEqual([1, true, null]);
-        expect(messages[3]).toEqual([2, false, 'Unknown api \'fn2\'']);
-        expect(messages[4]).toEqual([3, true, 9]);
-        expect(messages[5]).toEqual([4, false, 'Echo Error']);
-        expect(socket.onerror.mock.calls.length).toBe(0);
-        expect(session.url).toBe('/');
-
-        resolve();
-      });
     });
-  });
+
+    socket.on('open', () => {
+      send([1, 'fn1', [1, 2, 3]]);
+      send([2, 'fn2', [4]]);
+      send([3, 'sum', [4, 5]]);
+      send([4, 'err', ['Echo Error']]);
+    });
+
+    socket.on('close', () => {
+      expect(typeof session.dispatch).toBe('function');
+      expect(typeof session.emit).toBe('function');
+
+      expect(api.fn1.mock.calls.length).toBe(1);
+      const messages = socket.onmessage.mock.calls.map(c => JSON.parse(c[0].data));
+      messages.sort((a, b) => a[0] - b[0]);
+      expect(messages.length).toBe(6);
+      expect(messages[0]).toEqual([-1, 'eee', 'John']);
+      expect(messages[1]).toEqual([0, { type: 'ACTION', payload: 'Doe' }]);
+      expect(messages[2]).toEqual([1, true, null]);
+      expect(messages[3]).toEqual([2, false, 'Unknown api \'fn2\'']);
+      expect(messages[4]).toEqual([3, true, 9]);
+      expect(messages[5]).toEqual([4, false, 'Echo Error']);
+      expect(socket.onerror.mock.calls.length).toBe(0);
+      expect(session.url).toBe('/');
+
+      resolve();
+    });
+  }));
 
   it('must handle error', async () => new Promise((resolve) => {
     const socket = new WebSocket(`ws://localhost:${port}/err`);
