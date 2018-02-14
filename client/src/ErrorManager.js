6045648828e8c2d0574a16b373d54979678af7c3
Ranjan Shrestha <ranjan@sharingapples.com>
2018-02-14 13:41:22 +0545

    Categorize the error to - CONNECT, VALIDATE, FROZEN and GENERIC

--- client/src/ErrorManager.js
+++ client/src/ErrorManager.js
@@ -1,33 +1,51 @@
+const CONNECT = 'CONNECT';
+const VALIDATION = 'VALIDATION';
+const FROZEN = 'FROZEN';
+const GENERIC = 'GENERIC';
+
 module.exports = function createErrorManager(emit, connect, retryInterval = 3000) {
   let errorTimer = null;
-  let error = null;
+  let currentError = null;
 
   const retry = () => {
-    error = null;
+    currentError = null;
     errorTimer = null;
     connect();
   };
 
-  return {
-    get: () => error,
-    set: (code, message, interval = retryInterval) => {
-      if (errorTimer) {
-        clearTimeout(errorTimer);
-        errorTimer = null;
-      }
-      error = { code, message };
-      emit(error);
+  const setError = (type, error, interval = retryInterval) => {
+    if (errorTimer) {
+      clearTimeout(errorTimer);
+      errorTimer = null;
+    }
+    currentError = { type, error };
+    emit(error);
 
-      if (interval > 0) {
-        errorTimer = setTimeout(retry, interval);
-      }
+    if (interval > 0) {
+      errorTimer = setTimeout(retry, interval);
+    }
+  };
+
+  return {
+    get: () => currentError,
+    setConnectError: (error, interval) => {
+      setError(CONNECT, error, interval);
+    },
+    setValidationError: (error, interval) => {
+      setError(VALIDATION, error, interval);
+    },
+    setGenericError: (error, interval) => {
+      setError(GENERIC, error, interval);
+    },
+    setFrozenError: () => {
+      setError(FROZEN, null, 0);
     },
     clear: () => {
       if (errorTimer) {
         clearTimeout(errorTimer);
         errorTimer = null;
       }
-      error = null;
+      currentError = null;
     },
   };
 };
