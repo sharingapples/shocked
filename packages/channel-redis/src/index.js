322af495186707adb3905475451c72ee06880527
Ranjan Shrestha <ranjan@sharingapples.com>
2018-05-23 12:21:39 +0545

    Provide unsubscribe method

--- packages/channel-redis/src/index.js
+++ packages/channel-redis/src/index.js
@@ -16,6 +16,7 @@ export default function createProvider(redisOptions) {
   });
 
   return {
+    // TODO: In case the session is already subscribed return false
     subscribe: (channelId, session) => {
       const list = channels[channelId];
       if (!list) {
@@ -25,6 +26,27 @@ export default function createProvider(redisOptions) {
       } else {
         list.push(session);
       }
+      return true;
+    },
+
+    unsubscribe: (channelId, session) => {
+      const list = channels[channelId];
+      if (!list) {
+        return false;
+      }
+
+      const idx = list.indexOf(session);
+      if (idx === -1) {
+        return false;
+      }
+
+      list.splice(idx, 1);
+      // Cleanup
+      if (list.length === 0) {
+        delete channels[channelId];
+        client.unsubscribe(channelId);
+      }
+      return true;
     },
 
     publish: (channelId, message) => {
