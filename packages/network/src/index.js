de72fdc7d3b0bfd52e90e72092ed38e3cd0a17af
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-15 10:42:05 +0545

    Use object based approach with DNS resolver

--- packages/network/src/index.js
+++ packages/network/src/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
+import { Resolver } from 'dns';
 
-const dns = require('dns');
 const EventEmitter = require('events');
 
 const ONLINE = 'online';
@@ -8,65 +8,97 @@ const OFFLINE = 'offline';
 
 const DEFAULT_DOMAIN = 'google.com';
 const DEFAULT_POLL_INTERVAL = 5000;
+const DEFAULT_TIMEOUT_INTERVAL = 3000;
 
 const defaultConfig = {
   domain: DEFAULT_DOMAIN,
   poll: DEFAULT_POLL_INTERVAL,
+  timeout: DEFAULT_TIMEOUT_INTERVAL,
+  servers: null,
 };
-
-function poll(network) {
-  dns.resolve(network.domain, (err) => {
-    // Use the call back response only if the network check is active
-    if (network.active) {
-      const timerHandle = setTimeout(() => poll(network), network.pollInterval);
-      if (err) {
-        network._updateState(false, timerHandle);
-      } else {
-        network._updateState(true, timerHandle);
-      }
-    }
-  });
-}
 
 class Network extends EventEmitter {
   constructor(config) {
     super();
-    this.domain = config.domain || DEFAULT_DOMAIN;
-    this.pollInterval = config.poll || DEFAULT_POLL_INTERVAL;
+    this._domain = config.domain || DEFAULT_DOMAIN;
+    this._pollInterval = config.poll || DEFAULT_POLL_INTERVAL;
+    this._timeoutInterval = config.timeout || DEFAULT_TIMEOUT_INTERVAL;
+    this._servers = config.servers || null;
 
-    this.connected = null;
-    this.timerHandle = null;
-    this.active = false;
+    this._resolver = null;
+    this._connected = null;
+    this._timerHandle = null;
 
     // By default start the detector
     this.start();
   }
 
-  _updateState(status, timerHandle) {
-    this.timerHandle = timerHandle;
-    if (this.connected !== status) {
-      this.connected = status;
-      this.emit(status ? ONLINE : OFFLINE);
+  _updateState(status) {
+    if (this._connected !== status) {
+      this._connected = status;
+
+      super.emit(status ? ONLINE : OFFLINE);
     }
   }
 
   isConnected() {
-    return this.connected;
+    return this._connected;
   }
 
   start() {
-    if (!this.active) {
-      this.active = true;
-      poll(this);
+    if (this._resolver !== null) {
+      throw new Error('Network could be started only once');
     }
+
+    // Create the resolver
+    this._resolver = new Resolver();
+    if (this._servers) {
+      this._resolver.setServers(this._servers);
+    }
+
+    // Starts polling
+    this._poll();
   }
 
   stop() {
-    if (this.active) {
-      this.active = false;
-      clearTimeout(this.timerHandle);
+    if (this._resolver) {
+      this._resolver.cancel();
+      this._resolver = null;
+
+      // Also clear the poll handle
+      clearTimeout(this._timerHandle);
     }
+  }
+
+  _poll() {
+    // A timeout to wait for a dns resolution, will declare the network as offline if
+    // the query is not resolved within this period
+    const timeout = setTimeout(() => {
+      this._updateState(false);
+    }, this._timeoutInterval);
+
+    this._resolver.resolve(this._domain, (err) => {
+      // Clear the timeout interval
+      clearTimeout(timeout);
+      // Update the connection state
+      if (err) {
+        if (err.code === 'ECANCELLED') {
+          // If the resolution was cancelled, happens only when the network is stopped
+          // Just leave without doing anything
+          return;
+        }
+
+        this._updateState(false);
+      } else {
+        this._updateState(true);
+      }
+
+      // Once resolved, try again after poll interval
+      this._timerHandle = setTimeout(() => this._poll(), this._pollInterval);
+    });
   }
 }
 
-module.exports = (config = defaultConfig) => new Network(config);
+export default function createNetwork(config = defaultConfig) {
+  return new Network(config);
+}
