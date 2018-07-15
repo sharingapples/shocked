2fcfdb8c955aebcc4b5c5bbe62e60b65e4981c82
Ranjan Shrestha <ranjan@sharingapples.com>
2018-07-15 16:16:33 +0545

    Describe the api while creating scope

--- packages/server/src/scoping.js
+++ packages/server/src/scoping.js
@@ -6,41 +6,18 @@ export function createScope(name, initFn) {
     throw new Error(`A scope with the name ${name} has already been created`);
   }
 
-  const scope = {
-    apis: {},
-    init: initFn,
-  };
+  if (typeof initFn !== 'function') {
+    throw new Error(`Scope ${name} must be initialized with a function`);
+  }
 
-  scopes[name] = scope;
-
-  return (api, apiName) => {
-    const apiId = apiName || api.name;
-    if (!apiId) {
-      throw new Error(`Invalid api name under ${name} scope`);
-    }
-
-    if (scope.apis[apiId]) {
-      throw new Error(`Can't define multiple apis with the same id ${name}/${apiId}`);
-    }
-
-    scope.apis[apiId] = api;
-  };
+  scopes[name] = initFn;
 }
 
 export function getScope(scopeId, session) {
-  const scope = scopes[scopeId];
-  if (!scope) {
+  const init = scopes[scopeId];
+  if (!init) {
     throw new Error(`Scope ${scopeId} is not registered`);
   }
 
-  // First try to initialize the scope
-  if (scope.init) {
-    scope.init(session);
-  }
-
-  // Bind the session to all the scope apis
-  return Object.keys(scope.apis).reduce((res, name) => {
-    res[name] = scope.apis[name](session);
-    return res;
-  }, {});
+  return init(session);
 }
