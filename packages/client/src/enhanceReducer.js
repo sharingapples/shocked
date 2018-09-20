32991f9fc6d4b1a8779b939e2577dd9276ba35d7
Ranjan Shrestha <ranjan@sharingapples.com>
2018-09-20 09:56:51 +0545

    Allow the app reducer to intercept (override) the INIT action

--- packages/client/src/enhanceReducer.js
+++ packages/client/src/enhanceReducer.js
@@ -3,14 +3,18 @@ import { TYPE_BATCH_ACTIONS } from './batchActions';
 
 export default function enhanceReduce(reducer) {
   return function enhancedReducer(state = null, action) {
-    if (action.type === TYPE_TRACKER_INIT) {
-      return action.payload;
-    }
-
     if (action.type === TYPE_BATCH_ACTIONS) {
       return action.payload.reduce(enhancedReducer, state);
     }
 
-    return reducer(state, action);
+    const result = reducer(state, action);
+
+    // Special case, if the tracker init isn't handled by the reducer
+    // handle it by default
+    if (action.type === TYPE_TRACKER_INIT && result === state) {
+      return action.payload;
+    }
+
+    return result;
   };
 }
