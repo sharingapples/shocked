9ab787bf11a42fba111c351ed6f3d75bc29cdcee
Ranjan Shrestha <ranjan@sharingapples.com>
2018-10-29 15:56:52 +0545

    Support passing array for batch reduction

--- packages/client/src/enhanceReducer.js
+++ packages/client/src/enhanceReducer.js
@@ -6,6 +6,10 @@ export default function enhanceReduce(reducer) {
       return action.payload.reduce(enhancedReducer, state);
     }
 
+    if (Array.isArray(action)) {
+      return action.reduce(enhancedReducer, state);
+    }
+
     const result = reducer(state, action);
 
     // Special case, if the tracker init isn't handled by the reducer
