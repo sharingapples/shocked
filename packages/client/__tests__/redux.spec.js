888f5d84de5c5fd52bd7d592f1e44c826f26d7d1
Ranjan Shrestha <ranjan@sharingapples.com>
2019-03-19 17:05:48 +0545

    Fix test spec error

--- packages/client/__tests__/redux.spec.js
+++ packages/client/__tests__/redux.spec.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from 'redux';
 import WebSocket from 'isomorphic-ws';
-import { enhancer, getConnectivity, shockedReducer } from '../src';
+import { enhancer, setUrl } from '../src';
 
 const createServer = require('shocked-server');
 
@@ -16,7 +16,6 @@ const simpleReducer = (state = {}, action) => {
 };
 
 const reducer = combineReducers({
-  sh: shockedReducer,
   simple: simpleReducer,
 });
 
@@ -28,18 +27,18 @@ beforeAll(async () => {
   port = await server.listen();
 });
 
-function storeOnChange(store) {
-  return new Promise((resolve) => {
-    const prevState = store.getState();
-    const u = store.subscribe(() => {
-      const newState = store.getState();
-      if (newState !== prevState) {
-        u();
-        resolve(newState);
-      }
-    });
-  });
-}
+// function storeOnChange(store) {
+//   return new Promise((resolve) => {
+//     const prevState = store.getState();
+//     const u = store.subscribe(() => {
+//       const newState = store.getState();
+//       if (newState !== prevState) {
+//         u();
+//         resolve(newState);
+//       }
+//     });
+//   });
+// }
 
 afterAll(() => {
   server.close();
@@ -47,11 +46,13 @@ afterAll(() => {
 
 describe('redux binding specifications', () => {
   it('checks for standard redux usage', async () => {
-    const store = createStore(reducer, enhancer(`http://localhost:${port}`, null, { WebSocket }));
+    const store = createStore(reducer, enhancer({ WebSocket }));
+    setUrl(`http://localhost:${port}`);
+
     store.dispatch({ type: 'simple', payload: 'Foo' });
     expect(store.getState().simple.value).toBe('Foo');
-    expect(getConnectivity(store.getState().sh)).toBeUndefined();
 
-    expect((await storeOnChange(store)).sh.connectivity).toBe('rejected');
+    // TODO: Will need to test the shocked reducer directly
+    // expect((await storeOnChange(store)).sh.connectivity).toBe('rejected');
   });
 });
