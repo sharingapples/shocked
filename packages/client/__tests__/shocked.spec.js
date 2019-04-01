f3c9089476ae1c20f5e95b372c9365f6eb111922
Ranjan Shrestha <ranjan@sharingapples.com>
2019-04-01 12:42:58 +0545

    Remove test specs from older version

---
+++ packages/client/__tests__/shocked.spec.js
@@ -0,0 +1,5 @@
+describe('Shocked Test Specification', () => {
+  it('checks nothing', () => {
+    console.warn('No tests written yet. Write few tests.');
+  });
+});
--- packages/debug-client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
