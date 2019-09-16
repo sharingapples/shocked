d8cfa1a46cf307f1302360fb42b6213a92e3e9ba
Ranjan Shrestha <ranjan@sharingapples.com>
2019-09-16 16:14:35 +0545

    Support gracefully shutting down the server

--- packages/server/src/Server.ts
+++ packages/server/src/Server.ts
@@ -1,5 +1,6 @@
-import { App, TemplatedApp } from 'uWebSockets.js';
+import { App, TemplatedApp, us_listen_socket_close } from 'uWebSockets.js';
 import { Tracker, TrackerBehaviour } from './Tracker';
+import { Unsubscribe } from 'shocked-types';
 
 class Server<U> {
   app: TemplatedApp;
@@ -16,11 +17,11 @@ class Server<U> {
     return this;
   }
 
-  async start(port: number): Promise<number> {
+  async start(port: number): Promise<Unsubscribe> {
     return new Promise((resolve, reject) => {
       this.app.listen(port, (socket) => {
         if (socket) {
-          resolve(port);
+          resolve(() => us_listen_socket_close(socket));
         } else {
           reject(new Error(`Could not listen on port ${port}`));
         }
