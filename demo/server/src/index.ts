d3260250470593e066e3e0d5ca28a5b55d55283e
Ranjan Shrestha <ranjan@sharingapples.com>
2019-09-16 22:09:46 +0545

    Include params in demo

--- demo/server/src/index.ts
+++ demo/server/src/index.ts
@@ -1,35 +1,42 @@
 import { Server, Session } from 'shocked-server';
+import { HttpRequest } from 'uWebSockets.js';
 
 type User = {
   id: string,
   name: string,
 };
 
+type DemoSession = Session<User, string>;
+
 const demoApi = {
-  add: ([a, b]: [number, number], session: Session<User>) => {
+  add: ([a, b]: [number, number], session: DemoSession) => {
     session.dispatch(a * b);
     // Simulate processing
     return new Promise((resolve) => {
       setTimeout(() => resolve(a + b), 1000);
     });
   },
-  echo: (msg: any, session: Session<User>) => msg,
+  echo: (msg: any, session: DemoSession) => msg,
 };
 
-const server = new Server<User>();
+const server = new Server<User, string>();
 server.track('/a', {
   api: demoApi,
-  onIdent: (token: string) => {
+  preprocess: (req: HttpRequest) => {
+    return req.getUrl();
+  },
+  onIdent: (token: string, params: string) => {
     return new Promise((resolve, reject) => {
       if (token !== 'demo') {
         reject(new Error('Invalid identity. Use demo'));
       } else {
-        resolve({ id: token, name: token });
+        resolve({ id: token, name: params });
       }
     });
   },
   onStart: async (session) => {
     console.log('Session started');
+    session.dispatch({ user: session.user, params: session.params });
   },
 });
 
